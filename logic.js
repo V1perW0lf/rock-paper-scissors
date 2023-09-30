5885e5c547ecf6eafc5d08103ce3e56d7d9fd1f7
CJ <11570429+V1perW0lf@users.noreply.github.com>
2023-09-30 16:49:16 -0500

    add game logic, allowing for five rounds to be played with scores

--- logic.js
+++ logic.js
@@ -5,12 +5,30 @@ function getComputerChoice() {
 
 function playOneRound(playerSelection, computerSelection) {
     if(playerSelection == computerSelection) {
-        return `This round is a tie!`
+        return [`This round is a tie!`, 0]
     } else if (playerSelection == "Rock" && computerSelection == "Paper" || 
                playerSelection == "Paper" && computerSelection == "Scissors" ||
                playerSelection == "Scissors" && computerSelection == "Rock") {
-        return `You lose! ${computerSelection} beats ${playerSelection}!`
+        return [`You lose! ${computerSelection} beats ${playerSelection}!`, -1]
     } else {
-        return `You Win! ${playerSelection} beats ${computerSelection}!`
+        return [`You Win! ${playerSelection} beats ${computerSelection}!`, 1]
+    }
+}
+
+function game() {
+    let score = 0;
+    for (let i = 0; i < 5; i++) {
+        let input = prompt("What is your selection?", "")
+        let selection = input.charAt(0).toUpperCase() + input.slice(1);
+        let result = playOneRound(selection, getComputerChoice())
+        score += result[1];
+        console.log(result[0]);
+    }
+    if(score > 0) {
+        console.log("You win!");
+    } else if (score == 0) {
+        console.log("It's a tie!");
+    } else {
+        console.log("You lose!");
     }
 }
