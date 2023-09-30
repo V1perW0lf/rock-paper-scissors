5b536fb5012466988580bf2af977a164ad4517fe
CJ <11570429+V1perW0lf@users.noreply.github.com>
2023-09-30 16:26:00 -0500

    add round decision logic

--- logic.js
+++ logic.js
@@ -2,3 +2,15 @@ function getComputerChoice() {
     const computer_choices = ["Rock", "Paper", "Scissors"];
     return computer_choices[Math.floor(Math.random() * 3)];
 }
+
+function playOneRound(playerSelection, computerSelection) {
+    if(playerSelection == computerSelection) {
+        return `This round is a tie!`
+    } else if (playerSelection == "Rock" && computerSelection == "Paper" || 
+               playerSelection == "Paper" && computerSelection == "Scissors" ||
+               playerSelection == "Scissors" && computerSelection == "Rock") {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`
+    } else {
+        return `You Win! ${playerSelection} beats ${computerSelection}!`
+    }
+}
